deac21528f7989722d4ca6c25a8ee20ccce9d266
Martin Lohmer <M.Lohmer@web.de>
2018-07-29 20:51:22 +0200

    Nachtrag -Auskommentierungen-

--- Bewegen.cs
+++ Bewegen.cs
@@ -1,21 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Bewegen : MonoBehaviour {
-
-	// Use this for initialization
-	public Transform[] target;
-	public float speed;
-
-	private int current;
-
-	void Update() {
-		if (transform.position != target [current].position) {
-			Vector3 pos = Vector3.MoveTowards (transform.position, target [current].position, speed * Time.deltaTime);
-			GetComponent<Rigidbody> ().MovePosition (pos);
-		}  else
-			current = (current + 1) % target.Length;
-	}
-}
-	
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class Bewegen : MonoBehaviour {  	// Bei dieser Funktion werden Ziele erzeugt, die ein GameObject ansteuert. Es wird zusäzlich eine Variable fü die Geschwindigkeit des Objekts, welches diese 	// FUnktion nutzt, erzeugt.  	public Transform[] target; 	public float speed;  	private int current;  	//Bei jedem Update wird üerprüt, ob sich das Objekt bereits am Ziel befindet, oder sich eine Einheit weiterbewegen soll.  	void Update() { 		if (transform.position != target [current].position) { 			Vector3 pos = Vector3.MoveTowards (transform.position, target [current].position, speed * Time.deltaTime); 			GetComponent<Rigidbody> ().MovePosition (pos); 		}   else 			current = (current + 1) % target.Length; 	} } 
